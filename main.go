b0bc7bf66b373752b4ccbb7f028034b395f388cd
Akshay Arora <akshay@renderdragon.com>
2016-03-11 13:27:52 -0500

    Take in CLI args

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"bufio"
-	_ "bytes"
+	"flag"
 	"fmt"
 	"io"
 	"os/exec"
 	"regexp"
+	"strings"
 )
 
 var (
@@ -51,15 +52,41 @@ var (
 	BackgroundLightWhite   string = "\x1b[107m"
 )
 
-func main() {
-	text := "%scolored %s%stext%s\n"
-	fmt.Printf(text, ForegroundRed, ForegroundBlack, BackgroundRed, Reset)
-	fmt.Printf(text, ForegroundGreen, ForegroundMagenta, BackgroundGreen, Reset)
+var filterArg string
+var deviceFlag bool
+var deviceSerial string
 
-	//cmd := exec.Command("adb", `-d logcat -v time "*:S AndroidRuntime:E ZSE:*"`)
-	//cmd := exec.Command("adb", "devices")
-	//cmd := exec.Command("adb", "-s", "00bc1e18894429b5", "logcat", "-v", "time", "\"*:S AndroidRuntime:E ZSE:*\"")
-	cmd := exec.Command("adb", "-s", "00bc1e18894429b5", "logcat", "-v", "time")
+type LogLevel int
+
+const (
+	Verbose LogLevel = iota
+	Debug
+	Info
+	Warn
+	Error
+	Assert
+	Silent
+
+	ALL_APPS_LABEL string = "*"
+)
+
+func init() {
+	flag.StringVar(&filterArg, "filter", "", "Filters to use for adb, e.g. \"*:S AndroidRuntime:E YourApp:*\"")
+	flag.BoolVar(&deviceFlag, "d", true, "Read logcat from device")
+	flag.StringVar(&deviceSerial, "s", "", "Serial Number for the Device to read logcat from")
+}
+
+func main() {
+	flag.Parse()
+	var args []string
+	if deviceFlag {
+		args = append(args, "-d")
+	}
+	if len(deviceSerial) > 0 {
+		args = append(args, []string{"-s", deviceSerial}...)
+	}
+	args = append(args, []string{"logcat", "-v", "time"}...)
+	cmd := exec.Command("adb", args...)
 	out, err := cmd.StdoutPipe()
 	if err != nil {
 		fmt.Errorf(err.Error())
@@ -69,11 +96,36 @@ func main() {
 		fmt.Errorf(err.Error())
 		return
 	}
-	printOutput(out)
+	var filters map[string]LogLevel
+	if len(filterArg) > 0 {
+		filters = parseFilters(filterArg)
+	}
+	printOutput(out, filters)
 	cmd.Wait()
 }
 
-func printOutput(out io.ReadCloser) {
+func parseFilters(s string) map[string]LogLevel {
+	args := strings.Split(s, " ")
+	filters := make(map[string]LogLevel)
+	for _, arg := range args {
+		parts := strings.Split(arg, ":")
+		filters[parts[0]] = parseLogLevel(parts[1])
+	}
+	return filters
+}
+
+func shouldLog(ll LogLevel, label string, filters map[string]LogLevel) bool {
+	level, ok := filters[label]
+	if !ok {
+		level, ok = filters[ALL_APPS_LABEL]
+		if !ok {
+			return false
+		}
+	}
+	return ll >= level
+}
+
+func printOutput(out io.ReadCloser, filters map[string]LogLevel) {
 	in := bufio.NewScanner(out)
 	//                        1 = timestamp                         2=LogLevel 3=Label 4=PID 5=Log
 	re := regexp.MustCompile(`(\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3}) ([A-Z])\/(\S*)\s*\((\d*)\): (.*)`)
@@ -81,21 +133,78 @@ func printOutput(out io.ReadCloser) {
 		line := in.Text()
 		if re.MatchString(line) {
 			matches := re.FindStringSubmatch(line)
-			time := matches[1]
-			level := matches[2]
+			level := parseLogLevel(matches[2])
 			label := matches[3]
-			pid := matches[4]
-			message := matches[5]
-			fg := ForegroundWhite
-			switch level {
-			case "E":
-				fg = ForegroundRed
-			case "W":
-				fg = ForegroundYellow
-			}
-			if "AndroidRuntime" == label || "ZSE" == label {
+			if shouldLog(level, label, filters) {
+				time := matches[1]
+				pid := matches[4]
+				message := matches[5]
+				fg := colorForLevel(level)
 				fmt.Printf("%s%s %s/%s (%s):%s %s\n", fg, time, level, label, pid, message, Reset)
 			}
+		} else {
+			fmt.Printf("%s%s%s\n", ForegroundRed, line, Reset)
 		}
+	}
+}
+
+func colorForLevel(ll LogLevel) string {
+	switch ll {
+	case Verbose:
+		return ForegroundDefault
+	case Debug:
+		return ForegroundGreen
+	case Info:
+		return ForegroundCyan
+	case Warn:
+		return ForegroundYellow
+	case Error:
+		return ForegroundRed
+	case Assert:
+		return ForegroundRed
+	default:
+		return ForegroundDefault
+	}
+}
+
+func parseLogLevel(s string) LogLevel {
+	switch s {
+	case "V", "*":
+		return Verbose
+	case "D":
+		return Debug
+	case "I":
+		return Info
+	case "W":
+		return Warn
+	case "E":
+		return Error
+	case "A":
+		return Assert
+	case "S":
+		return Silent
+	default:
+		return Silent
+	}
+}
+
+func (l LogLevel) String() string {
+	switch l {
+	case Silent:
+		return "S"
+	case Verbose:
+		return "V"
+	case Debug:
+		return "D"
+	case Info:
+		return "I"
+	case Warn:
+		return "W"
+	case Error:
+		return "E"
+	case Assert:
+		return "A"
+	default:
+		return "S"
 	}
 }
